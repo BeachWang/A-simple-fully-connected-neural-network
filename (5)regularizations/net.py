e8d5a550e40a8d3afbe4614e3c5b10fadf9b327d
BeachWang <1400012807@pku.edu.cn>
2019-11-02 14:10:42 +0800

    Create net.py

---
+++ (5)regularizations/net.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 11 21:05:00 2019
+
+@author: beach
+"""
+
+import torch.nn as nn
+
+class SimpleNet(nn.Module):
+    def __init__(self, in_dim, n_hidden_1, n_hidden_2, out_dim):
+        super(SimpleNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Linear(in_dim, n_hidden_1),
+            nn.BatchNorm1d(n_hidden_1),
+            nn.ReLU(True))
+        self.layer2 = nn.Sequential(
+            nn.Linear(n_hidden_1, n_hidden_2),
+            nn.BatchNorm1d(n_hidden_2),
+            nn.ReLU(True))
+        self.layer3 = nn.Sequential(nn.Linear(n_hidden_2, out_dim))
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        return x
